cd58bf021baf811d401d3a48135d2243c76eaa2a
Ensar27 <ensarozerden123@gmail.com>
2023-05-08 18:08:44 +0300

    V3

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -3,6 +3,7 @@ public class Class01 {
     public static void main(String[] args) {
         //V1 çalışmaları ...
         //V2 üzerinde çalışmalar
+        System.out.println("V^3 kodları");//V3
 
 
 
